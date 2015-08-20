aac608b56dd8ef9a3db90e213ee18c13d1e1e168
eferdman <eferdman2@gmail.com>
2015-08-20 15:34:26 -0700

    Add Google Street View

--- js/script.js
+++ js/script.js
@@ -11,7 +11,21 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    // Grab the user's input
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var address = street + ', ' + city;
+
+    // Update the greeting
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    // Create a link to the address' Google Street View
+    var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' +
+    address + '';
+
+    // Add the image to the page's body
+    $body.append('<img class="bgimg" src="' + streetviewURL + '">');
+
 
     return false;
 };
